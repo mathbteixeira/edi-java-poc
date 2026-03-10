2d283e933676c55ac0891c872d18dcb0fe798fa8
Matheus Batista Teixeira <mathbteixeira@gmail.com>
2026-03-09 22:24:04 -0300

    Initial commit

---
+++ src/main/java/com/mathbteixeira/edi_poc/EdiPocApplication.java
@@ -0,0 +1,13 @@
+package com.mathbteixeira.edi_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdiPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdiPocApplication.class, args);
+	}
+
+}
